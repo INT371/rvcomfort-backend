45e10a3ee5d1246df840063e21db5af11be41d9f
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2022-04-10 14:51:58 +0700

    init project - first commit

---
+++ src/main/java/com/example/demo/RvcomfortApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvcomfortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvcomfortApplication.class, args);
+	}
+
+}
